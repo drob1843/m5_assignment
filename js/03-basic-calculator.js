63920cf9b9cdf7c7dac7804a6923ca89fa652f6a
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-10-15 20:27:06 -0700

    last

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,9 +1,50 @@
 // ADD A FUNCTION CALLED CALCULATE
+function calculate(x, y, operator) {
+    let result
 
-// COLLECT FIRST NUMBER FROM USER
+    // COLLECT FIRST NUMBER FROM USER
 
-// COLLECT SECOND NUMBER FROM USER
+    do {
+        x = parseFloat(prompt(`Choose the first number`))
 
-// COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+    } while (isNaN(x))
 
-// CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+
+
+    // COLLECT SECOND NUMBER FROM USER
+
+    do {
+        y = parseFloat(prompt(`choose your second value`))
+    } while (isNaN(y))
+
+    // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+
+    do {
+        operator = prompt(`Choose an operator: add, subtract, multiply or divide`).toLowerCase()
+    } while (operator !== 'add' && operator !== 'subtract' && operator !== 'multiply' && operator !== 'divide')
+
+    // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+
+    if (operator === 'add') {
+        alert(result = x + y)
+    } else if (operator === 'subtract') {
+        alert(result = x - y)
+    } else if (operator === 'multiply') {
+        alert(result = x * y)
+    } else if (operator === 'divide') {
+
+        if (x === 0 && y === 0) {
+            alert(`you cannot divide 0 by 0`)
+        }
+        else if (y === 0){
+            alert(`Your answer is undefined`)
+
+        } else {
+        alert(result = x / y)}
+
+    }
+}
+
+
+
+calculate()
